7b93420b4fc99743e10664c7c2c7f3503db8dfb4
Ish Bosamiya <ishbosamiya@gmail.com>
2018-11-12 19:26:02 +0530

    Ray Triangle Intersection Working!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from hit_record import Hit_Record
 from ppm_writer import ppmWriter
 from camera import Camera
 from sphere import Sphere
+from triangle import Triangle
 
 from math import sqrt
 
@@ -12,7 +13,8 @@ height = 200
 no_of_samples = 10
 
 pixels = []
-spheres = [Sphere(Vec3(-0.5, 0.0, 2.0), 0.5), Sphere(Vec3(0.5, 0.0, 2.0), 0.5)]
+#spheres = [Sphere(Vec3(-0.5, 0.0, 2.0), 0.5), Sphere(Vec3(0.5, 0.0, 2.0), 0.5)]
+triangles = [Triangle(v = [Vec3(0.0, 1.0, 2.0), Vec3(1.0, -1.0, 2.0), Vec3(-1.0, -1.0, 2.0)])]
 
 camera_origin = Vec3(0.0, 0.0, 0.0)
 camera_length = 1.0
@@ -23,8 +25,8 @@ for y in range(height, 0, -1):
 		colour = Vec3(0, 0, 0)
 		for s in range(0, no_of_samples):
 			ray = camera.getRay(x, y)
-			for sphere in spheres:
-				temp = sphere.hit(ray, 0.0, 1000.0)
+			for triangle in triangles:
+				temp = triangle.hit(ray, 0.0, 1000.0)
 				if temp[0]:
 					colour = colour + ((temp[1].normal + Vec3(1.0, 1.0, 1.0)) / 2.0) * 255.0
 					break
--- triangle.py
+++ triangle.py
@@ -1,10 +1,9 @@
 from vec3 import Vec3
 from ray import Ray
-from Hitable import Hitable
 
-from math import abs
+from hit_record import Hit_Record
 
-class Triangle(Hitable):
+class Triangle():
 	def __init__(self, v, vt = [], vn = []):
 		self.v = v
 		self.vt = vt
@@ -14,11 +13,11 @@ class Triangle(Hitable):
 		edge1 = self.v[1] - self.v[0]
 		edge2 = self.v[2] - self.v[0]
 		
-		n = edge1.cross(edge2)
+		n = edge1.cross(edge2).normalized()
 		q = ray_in.getDirection().cross(edge2)
 		a = edge1.dot(q)
 
-		if n.dot(q) >= 0 or abs(a) <= 0.0001:
+		if n.dot(ray_in.getDirection()) >= 0.0 or abs(a) <= 0.0001:
 			return (False, hit_record)
 
 		s = (ray_in.getOrigin() - self.v[0]) / a
@@ -26,3 +25,16 @@ class Triangle(Hitable):
 		
 		b = [s.dot(q), r.dot(ray_in.getDirection())]
 		bar_coords = Vec3(b[0], b[1], 1.0 - b[0] - b[1])
+		
+		if bar_coords.x < 0.0 or bar_coords.y < 0.0 or bar_coords.z < 0.0:
+			return (False, hit_record)
+
+		t = edge2.dot(r)
+		if t >= 0:
+			hit_record.ray_in = ray_in
+			hit_record.point = ray_in.pointAtParameter(t)
+			hit_record.normal = n
+			hit_record.t = t
+			return (True, hit_record)
+		else:
+			return (False, hit_record)
