3e94ea3f1480046ebe2592dc3670ab755fbf38e0
Ish Bosamiya <ishbosamiya@gmail.com>
2018-11-13 19:47:19 +0530

    Model now has metal shader

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ pixels = []
 triangles = [Triangle(v = [Vec3(0.0, 1.0, 2.0), Vec3(1.0, -5.0, 5.0), Vec3(-1.0, -5.0, 0.0)], material = Lambert(Vec3(0.1, 0.2, 0.8)))]
 spheres = [Sphere(Vec3(0.60, 0.0, 2.0), 0.5, Lambert(Vec3(0.2, 0.9, 0.55))), Sphere(Vec3(0.5, -100.0, 2.0), 99.5, Lambert(Vec3(1.0, 1.0, 1.0))), Sphere(Vec3(-0.60, -0.2, 2.0), 0.5, Lambert(Vec3(0.89, 0.65, 0.55)))]
 hitable_list = Hitable_List(spheres + triangles)
-model = Model()
+model = Model(material = Metal(Vec3(0.8, 0.8, 0.82), 0.3))
 model.readObj("../temp_obj.obj")
 hitable_list = Hitable_List([model])
 
@@ -56,7 +56,7 @@ for y in range(height, 0, -1):
 		colour = Vec3(0, 0, 0)
 		for s in range(0, no_of_samples):
 			ray = camera.getRay(x, y)
-			colour = colour + rayTrace(hitable_list, ray, 1)
+			colour = colour + rayTrace(hitable_list, ray, 5)
 		pixels.append(colour/no_of_samples * 255.0)
 
 print("Actual length:", len(pixels), "Expected Length:", width * height)
